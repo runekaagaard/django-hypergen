112219315eece63a4150c4b5cdfb8ff00f4cf945
Rune Kaagaard <rumi.kg@gmail.com>
2021-01-25 21:08:34 +0100

    Thinking about concurrency.

---
+++ experiments/concurrency.js
@@ -0,0 +1,18 @@
+events:
+  id: input1    event: oninput  callback: autocomplete_items group: blockable throttle: 500
+  id: textarea1 event: onchange callback: spellcheck         group: whenever  debounce: 1000
+  id: button1   event: onclick  callback: save_data          group: save
+timers:
+  - callback: update_graph interval: 1200 group: whenever
+
+execution_groups:
+  whenever:
+  blockable:
+  save:
+    blocks: [blockable, save]
+    block_message: "Please wait until data is saved on the server"
+    clears_before_block: [blockable]
+  serial:
+    blocks: [save]
+    clears_before_block: [save]
+
