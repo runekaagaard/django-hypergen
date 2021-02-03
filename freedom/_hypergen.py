32a2a5f4cf0d2703d81b8ee7d960dc516d7458aa
Rune Kaagaard <rumi.kg@gmail.com>
2021-02-03 16:07:46 +0100

    Refactorings.

--- freedom/_hypergen.py
+++ freedom/_hypergen.py
@@ -35,212 +35,44 @@ else:
 
 
 ### Globals ###
-
-state = local()
-state.auto_id = False
-state.liveview = False
-UPDATE = 1
-
-
-def hypergen_context():
+def hypergen_context(**kwargs):
     from freedom.core import namespace as ns
     return ns(
         into=[],
         liveview=True,
         id_counter=base65_counter(),
         id_prefix=(freedom.loads(context.request.body)["id_prefix"]
-                   if context.request.is_ajax() else ""))
+                   if context.request.is_ajax() else ""),
+        event_handler_cache={},
+        target_id=kwargs.pop("target_id", "__main__"),
+        commands=[], )
 
 
 ### Control ###
 def hypergen(func, *args, **kwargs):
-    kwargs = deepcopy(kwargs)
-    auto_id = kwargs.pop("auto_id", True)
-    try:
-        if "request" in context:  # TODO
-            state.html = []
-            state.liveview = True
-            state.id_counter = base65_counter() if auto_id else None
-            state.id_prefix = kwargs.pop(
-                "id_prefix", (freedom.loads(context.request.body)["id_prefix"]
-                              if context.request.is_ajax() else ""))
-            state.auto_id = auto_id
-            # Key/value store sent to the frontend.
-            state.kv = {}
-            state.target_id = kwargs.pop("target_id", "__main__")
-            state.commands = []
-            func(*args, **kwargs)
-            html = "".join(
-                str(x()) if callable(x) else str(x) for x in state.html)
-            if state.kv:
-                state.commands.append([
-                    "./freedom", ["setEventHandlerCache"],
-                    [state.target_id, state.kv]
-                ])
-            if not context.request.is_ajax():
-                s = '<script>window.applyCommands({})</script>'.format(
-                    freedom.dumps(state.commands))
-                pos = html.find("</head")
-                if pos != -1:
-                    html = insert(html, s, pos)
-                return HttpResponse(html)
-            else:
-                state.commands.append(
-                    ["./freedom", ["morph"], [state.target_id, html]])
-                return state.commands
-
-    finally:
-        state.html = []
-        state.id_counter = None
-        state.id_prefix = ""
-        state.auto_id = False
-        state.liveview = False
-        state.kv = {}
-        state.target_id = None
-        state.commands = []
-
-    return html
-
-
-### Building HTML, internal API ###
-
-
-def _element_start_1(tag, attrs, into):
-    attrs = _sort_attrs(attrs)
-    raw(("<", tag), into=into)
-    return attrs
-
-
-def _element_start_2(k, v, into):
-    k = t(k).rstrip("_").replace("_", "-")
-    if type(v) is bool:
-        if v is True:
-            raw((" ", k), into=into)
-    elif k == "style" and type(v) in (dict, OrderedDict):
-        raw((" ", k, '="', ";".join(
-            t(k1) + ":" + t(v1) for k1, v1 in items(v)), '"'),
-            into=into)
-    else:
-        raw((" ", k, '="', t(v), '"'), into=into)
-
-
-def _element_start_3(children, into, void, sep):
-    if void:
-        raw(("/"), into=into)
-    raw(('>', ), into=into)
-    write(*children, into=into, sep=sep)
-
-
-def element_start(tag,
-                  children,
-                  lazy=False,
-                  into=None,
-                  void=False,
-                  sep="",
-                  add_to=None,
-                  js_cb="H.cbs.s",
-                  **attrs):
-    # TODO: Clean up this function. Only auto-id things with event handlers.
-    assert "add_to" not in attrs
-    if state.auto_id and "id_" not in attrs:
-        attrs["id_"] = state.id_prefix + next(state.id_counter)
-
-    attrs = _element_start_1(tag, attrs, into)
-
-    meta = {}
-    if state.liveview is True:
-        meta["this"] = "{}('{}')".format(js_cb, attrs["id_"])
-        meta["id"] = attrs["id_"]
-        meta["js_cb"] = js_cb
-    if into is not None:
-        into.meta = meta
-    for k, v in items(attrs):
-        if state.liveview is True and k.startswith("on") and type(v) in (
-                list, tuple):
-            tmp1 = v
-            tmp2 = lambda: _callback(tmp1, meta["this"])
-            raw(" ", k, '="', into=into)
-            raw(tmp2 if lazy else tmp2(), into=into)
-            raw('"', into=into)
+    with context(hypergen=hypergen_context(**kwargs)):
+        func(*args, **kwargs)
+        # TODO, use join_html
+        html = "".join(
+            str(x()) if callable(x) else str(x) for x in context.hypergen.into)
+        if context.hypergen.event_handler_cache:
+            context.hypergen.commands.append([
+                "./freedom", ["setEventHandlerCache"], [
+                    context.hypergen.target_id,
+                    context.hypergen.event_handler_cache
+                ]
+            ])
+        if not context.request.is_ajax():
+            s = '<script>window.applyCommands({})</script>'.format(
+                freedom.dumps(context.hypergen.commands))
+            pos = html.find("</head")
+            if pos != -1:
+                html = insert(html, s, pos)
+            return HttpResponse(html)
         else:
-            _element_start_2(k, v, into)
-
-    _element_start_3(children, into, void, sep)
-
-    blob = Blob(into, meta)
-    if add_to is not None:
-        add_to.append(blob)
-    return blob
-
-
-def element(tag,
-            children,
-            lazy=False,
-            into=None,
-            void=False,
-            sep="",
-            add_to=None,
-            **attrs):
-    blob = element_start(
-        tag,
-        children,
-        lazy=lazy,
-        into=into,
-        void=void,
-        sep="",
-        add_to=add_to,
-        **attrs)
-    element_end(tag, [], into=into, void=void)
-    return blob
-
-
-def element_ret(tag,
-                children,
-                lazy=False,
-                into=None,
-                void=False,
-                sep="",
-                add_to=None,
-                **attrs):
-    into = Blob()
-    element(
-        tag,
-        children,
-        lazy=lazy,
-        into=into,
-        void=void,
-        sep="",
-        add_to=add_to,
-        **attrs)
-
-    return into
-
-
-def element_end(tag, children, into=None, sep="", void=False):
-    if void is False:
-        write(*children, into=into, sep=sep)
-        raw(("</", t(tag), ">"), into=into)
-
-
-def element_con(tag, children, into=None, sep="", void=False, **attrs):
-    element = element_start(
-        tag, children, into=into, sep=sep, void=void, **attrs)
-    yield element
-    element_end(tag, [], into=into, void=void)
-
-
-def element_dec(tag, children, into=None, sep="", void=False, **attrs):
-    def _(f):
-        @wraps(f)
-        def __(*args, **kwargs):
-            element_start(
-                tag, children, into=into, sep=sep, void=void, **attrs)
-            f(*args, **kwargs)
-            element_end(tag, [], into=into, void=void)
-
-        return __
-
-    return _
+            context.hypergen.commands.append(
+                ["./freedom", ["morph"], [context.hypergen.target_id, html]])
+            return context.hypergen.commands
 
 
 ### Building HTML, public API ###
@@ -309,30 +141,6 @@ def t(s, quote=True):
     return escape(str(s), quote=quote)
 
 
-class Blob(object):
-    def __init__(self, html=None, meta=None):
-        self.html = html if html is not None else []
-        self.meta = meta
-
-    def append(self, x):
-        return self.html.append(x)
-
-    def extend(self, x):
-        return self.html.extend(x)
-
-    def pop(self, n=-1):
-        return self.html.pop(n)
-
-    def __getitem__(self, index):
-        return self.html[index]
-
-    def __str__(self):
-        return "".join(str(x) for x in self.html)
-
-    def __unicode__(self):
-        return "".join([str(x) for x in self.html])
-
-
 def base65_counter():
     # THX: https://stackoverflow.com/a/49710563/164449
     abc = letters + string.digits + "-_:"
@@ -358,75 +166,6 @@ class THIS(object):
 NON_SCALARS = set((list, dict, tuple))
 DELETED = ""
 
-
-def _callback(args, this, debounce=0):
-    func = args[0]
-    assert callable(func), ("First callback argument must be a callable, got "
-                            "{}.".format(repr(func)))
-    args = args[1:]
-
-    args2 = []
-    for arg in args:
-        if type(arg) in NON_SCALARS:
-            state.kv[id(arg)] = arg
-            args2.append(
-                freedom.quote("H.e['{}'][{}]".format(state.target_id, id(
-                    arg))))
-        else:
-            args2.append(arg)
-
-    return "H.cb({})".format(
-        freedom.dumps(
-            [func.hypergen_callback_url] + list(args2),
-            unquote=True,
-            escape=True,
-            this=this))
-
-
-class base_element(ContextDecorator):
-    js_cb = "H.cbs.s"
-    void = False
-
-    def __init__(self, *children, **attrs):
-        self.children = children
-        self.attrs = attrs
-        # self.i = len(state.html)
-        # for child in children:
-        #     if issubclass(type(child), base_element):
-        #         state.html[child.i] = DELETED
-
-        # state.html.append(
-        #     lambda: element_ret(self.tag, children, js_cb=self.js_cb, void=self.void, **attrs))
-        super(base_element, self).__init__()
-
-    def __enter__(self):
-        element_start(
-            self.tag,
-            self.children,
-            js_cb=self.js_cb,
-            void=self.void,
-            this=self,
-            **self.attrs)
-        state.html[self.i] = DELETED
-        return self
-
-    def __exit__(self, *exc):
-        return element_end(self.tag, [])
-
-    def __str__(self):
-
-        blob = element_ret(
-            self.tag,
-            self.children,
-            js_cb=self.js_cb,
-            void=self.void,
-            **self.attrs)
-        return "".join(blob.html)
-
-    def __unicode__(self):
-        return self.__str__()
-
-
 ### Input ###
 
 INPUT_TYPES = dict(
@@ -435,6 +174,10 @@ INPUT_TYPES = dict(
     number="H.cbs.i",
     range="H.cbs.f",
     week="H.cbs.i")
+
+
+class base_element(object):  # TODO: Delete
+    pass
 
 
 class input_(base_element):
