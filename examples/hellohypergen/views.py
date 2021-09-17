a3fb151e33368eca1aec12879204bfcc3951516a
Rune Kaagaard <rumi.kg@gmail.com>
2021-09-17 21:25:23 +0200

    refactoring

--- examples/hellohypergen/views.py
+++ examples/hellohypergen/views.py
@@ -1,5 +1,3 @@
-from contextlib import contextmanager
-
 from hypergen.core import *
 from hypergen.core import callback as cb
 from hypergen.contrib import hypergen_view, hypergen_callback, NO_PERM_REQUIRED
@@ -8,21 +6,16 @@ from django.templatetags.static import static
 
 @hypergen_view(perm=NO_PERM_REQUIRED)
 def counter(request):
-    with base_template():
-        template(1)
-
-@hypergen_callback(perm=NO_PERM_REQUIRED, target_id="body")
-def increment(request, n):
-    template(n + 1)
-
-@contextmanager
-def base_template():
     doctype()
     with html():
         with head():
             script(src=static("hypergen/hypergen.min.js"))
         with body(id_="body"):
-            yield
+            template(1)
+
+@hypergen_callback(perm=NO_PERM_REQUIRED, target_id="body")
+def increment(request, n):
+    template(n + 1)
 
 def template(n):
     label("Current value: ")
