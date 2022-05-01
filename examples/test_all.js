b1128882c787f7532bf0d002803eeeec2f63ed50
Rune Kaagaard <rumi.kg@gmail.com>
2022-05-01 19:09:14 +0200

    testcafe: Also click on stuff.

--- examples/test_all.js
+++ examples/test_all.js
@@ -8,7 +8,7 @@ fixture("Test All")
   .page(url)
   .requestHooks(logger);
 
-var queue = ["/"]
+var queue = ["/partialload/page1"]
 var visited = []
 
 async function visitPage(t, href) {
@@ -22,9 +22,28 @@ async function visitPage(t, href) {
   for (let i = 0; i < count; i++) {
     const link = await links.nth(i)
     const href2 = await link.getAttribute("href")
-    if (href2.startsWith("http") || visited.indexOf(href2) !== -1 || href2.startsWith("#")) continue
+    const onclick = await link.getAttribute("onclick")
+    if (href2.startsWith("http") || visited.indexOf(href2) !== -1 || href2.startsWith("#") || !!onclick) continue
     console.log(`    adding ${href2} to queue`)
     queue.push(href2)
+  }
+}
+
+async function onclickEvents(t, href) {
+  // TODO: Make todomvc work.
+  if (["/gameofcython/", "/t9n/", "/todomvc/"].indexOf(href) !== -1) return
+  const els = Selector('*').withAttribute('onclick')
+  const count = await els.count
+  for (let i = 0; i < count; i++) {
+    const el = await els.nth(i)
+    const onclick = await el.getAttribute("onclick")
+    if (onclick.includes("redirect__")) continue
+    console.log(`    running on click event ${onclick}`, i)
+    await t
+      .setNativeDialogHandler(() => true)
+      .click(el)
+      .expect(logger.contains(r => r.response.statusCode === 200)).ok()
+    logger.clear()
   }
 }
 
@@ -35,7 +54,8 @@ test('test', async t => {
     await t
       .navigateTo(href)
       .expect(logger.contains(r => r.response.statusCode === 200)).ok()
-    await visitPage(t, href)
     logger.clear()
+    await onclickEvents(t, href)
+    await visitPage(t, href)
   }
 })
