aa15129a284214025b9a99da03581228eb7397ca
Rune Kaagaard <rumi.kg@gmail.com>
2022-05-07 17:14:07 +0200

    Removing KV model to avoid cluttering users databases.

--- src/hypergen/models.py
+++ src/hypergen/models.py
@@ -1,13 +1,16 @@
 from django.db import models as m
+from django.conf import settings
 
-class KV(m.Model):
-    key = m.CharField(max_length=120, unique=True)
-    value = m.TextField()
+if getattr(settings, "HYPERGEN_ENABLE_INCUBATION", False) is True:
 
-    class Meta:
-        permissions = [('kv_hypergen_translations', 'Can edit translations')]
+    class KV(m.Model):
+        key = m.CharField(max_length=120, unique=True)
+        value = m.TextField()
 
-    def save(self, *args, **kwargs):
-        super(KV, self).save(*args, **kwargs)
-        from hypergen.core import set_translations
-        set_translations(self)
+        class Meta:
+            permissions = [('kv_hypergen_translations', 'Can edit translations')]
+
+        def save(self, *args, **kwargs):
+            super(KV, self).save(*args, **kwargs)
+            from hypergen.core import set_translations
+            set_translations(self)
