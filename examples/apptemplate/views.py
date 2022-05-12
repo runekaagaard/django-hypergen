04cb2dfefb9e2c26eedb8588a8b31f02ea0fdf6f
Rune Kaagaard <rumi.kg@gmail.com>
2022-05-12 20:55:57 +0200

    ra

--- examples/apptemplate/views.py
+++ examples/apptemplate/views.py
@@ -5,6 +5,7 @@ from hypergen.core import *
 from hypergen.contrib import hypergen_view, hypergen_callback, NO_PERM_REQUIRED
 
 from django.templatetags.static import static
+from website.templates import show_sources
 
 # Templates - as your app grows you probably want to move them to a templates.py file.
 
@@ -18,13 +19,12 @@ def base_template():
         with head():
             if title:
                 title("Hello apptemplate")  # arguments to elements becomes the html innerText of the element.
-            script(src=static(
-                "hypergen/hypergen.min.js"))  # keyword arguments becomes html attributes of the element.
+            script(
+                src=static("hypergen/hypergen.min.js"))  # keyword arguments becomes html attributes of the element.
             link("https://unpkg.com/simpledotcss@2.0.7/simple.min.css")  # include all you html5 boilerplate.
         with body():  # warning, don't set the target_id directly on the body element, does not work!
             h1("Hello apptemplate")
-            p(i("Congratulations on installing your very first Django Hypergen app!")
-             )  # elements can take elements.
+            p(i("Congratulations on installing your very first Django Hypergen app!"))  # elements can take elements.
 
             with div(id_="content"):  # see target_id below.
                 # The html triggered inside your views will appear here.
@@ -38,15 +38,17 @@ def base_template():
                     sep=" ",  # arguments are joined by a " " separator.
                 )
                 li("Read the", a("getting started", href="https://hypergen.it/gettingstarted/begin/"), "guide",
-                   sep=" ")
+                    sep=" ")
                 li("Check out the full", a("documentation", href="https://hypergen.it/documentation/"), sep=" ")
                 li("Drop by and", a("say hi", href="https://github.com/runekaagaard/django-hypergen/discussions"),
-                   "- we would love to talk to you!", sep=" ")
+                    "- we would love to talk to you!", sep=" ")
                 li(
                     "Submit ",
                     a("bug reports and feature requests",
-                      href="https://github.com/runekaagaard/django-hypergen/issues"))
+                    href="https://github.com/runekaagaard/django-hypergen/issues"))
                 li("Go crazy 24/7!")
+
+            show_sources(__file__)
 
 def content_template(encrypted_message=None):
     """
