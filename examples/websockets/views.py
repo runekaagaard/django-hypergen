d63c3465523e0236799681d04e6d3cc6797b34ec
Rune Kaagaard <rumi.kg@gmail.com>
2022-09-24 20:41:40 +0200

    revert

--- examples/websockets/views.py
+++ examples/websockets/views.py
@@ -42,15 +42,3 @@ def chat(request):
     input_(id_="message", type_="text", placeholder="Write your message here and press enter.", autofocus=True,
         onkeyup="myapp.sendChatMessage(event, 'message')")
     ul(id_="messages")
-
-def websocket(group_name=None, channel_name=None, target_id=None):
-    return lambda f: None
-
-N = 0
-
-@websocket(group_name="chat", channel_name="message", target_id="num-messages")
-def chat_message(msg):
-    global N
-    N += 1
-    p("You have", N, "messages in total.", sep=" ")
-    command("hypergen.append", "messages", hypergen(lambda: li(msg)))
