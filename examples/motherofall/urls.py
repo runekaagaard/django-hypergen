0a9cccfdf97854d90677b4195f50d9a2dd6efbc4
Rune Kaagaard <rumi.kg@gmail.com>
2022-09-30 14:19:43 +0200

    remove motherofall.

--- examples/motherofall/urls.py
+++
@@ -1,8 +0,0 @@
-from hypergen.hypergen import autourls
-from motherofall import views
-
-app_name = 'motherofall'
-
-# Automatically creates urlpatterns for all functions in views.py decorated with @liveview or @action.
-# If that's not your thing, normal urlpatterns works as well.
-urlpatterns = autourls(views, app_name)
